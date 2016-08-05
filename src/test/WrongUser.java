a2b412cc27eed8c006954a52c97a037da0fabb3c
Miroslav Stevanovic <Miroslav.Stevanovic@endava.com>
2016-08-05 15:53:21 +0200

    Domaci zadatak

---
+++ src/test/WrongUser.java
@@ -0,0 +1,54 @@
+package test;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.server.browserlaunchers.Sleeper;
+import org.testng.annotations.AfterClass;
+
+public class WrongUser {
+	
+	public static String username = "ivecluj12";
+    public static String password = "IveParola2";
+    
+  
+    public static final String URL = "http://www.endavauniversity.com";
+    
+    private static WebDriver driver;
+
+  @BeforeClass
+  	public void beforeClass() {
+	  
+	driver = new FirefoxDriver(); 
+  	driver.get(URL);
+  }
+  
+
+  @Test
+  	public void f() {
+	  
+	  driver.findElement(By.id("username")).sendKeys(username);
+	  driver.findElement(By.id("password")).sendKeys(password);
+	  Sleeper.sleepTightInSeconds(3);
+	  driver.findElement(By.id("loginbtn")).click();
+	  
+	  Sleeper.sleepTightInSeconds(5);
+	  String message = driver.findElement(By.className("error")).getText();
+	  System.out.println(message);
+	  
+	  assert message.contains("Invalid login") : "Error : You are logged in";
+	  
+	  System.out.println("Test passed");
+	  }
+	  
+	  
+	  
+  @AfterClass
+  public void afterClass() {
+	  
+	  driver.quit();
+  }
+
+}
